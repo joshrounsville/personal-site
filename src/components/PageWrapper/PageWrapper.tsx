4fb53966fb0c8e782e4247882afce552ddde7392
Josh Rounsville <joshuarounsville@gmail.com>
2022-06-29 23:22:24 -0700

    add favicons

--- src/components/PageWrapper/PageWrapper.tsx
+++ src/components/PageWrapper/PageWrapper.tsx
@@ -17,6 +17,10 @@ export function PageWrapper({
   bodyClassName,
   htmlClassName,
 }: IPageWrapperProps) {
+  const title = "Josh Rounsville # Front-end development"
+  const description =
+    "I'm a front-end developer living and working in Portland, Oregon."
+
   return (
     <>
       <Helmet
@@ -26,7 +30,17 @@ export function PageWrapper({
         bodyAttributes={{
           class: bodyClassName,
         }}
-      />
+      >
+        <title>{title}</title>
+
+        <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <meta name="apple-mobile-web-app-title" content={title} />
+        <meta name="msapplication-TileColor" content="#3372DF"></meta>
+      </Helmet>
       <main className={cx(styles.container, className)}>{children}</main>
     </>
   )
